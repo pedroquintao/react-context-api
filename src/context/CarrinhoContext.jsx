c5e26e91ee3421f2d44d45d69fd1d507f9437e17
pedroquintao <pedro.h.quintao@hotmail.com>
2024-06-17 22:40:29 -0300

    Versão final do curso

--- src/context/CarrinhoContext.jsx
+++ src/context/CarrinhoContext.jsx
@@ -1,20 +1,41 @@
+import { useEffect, useMemo, useReducer } from "react";
 import { createContext, useState } from "react";
+import { carrinhoReducer } from "../reducers/carrinhoReducer";
 
 export const CarrinhoContext = createContext();
 
 export const CarrinhoProvider = ({ children }) => {
 
-    const [carrinho, setCarrinho] = useState([]);
+    const estadoInicial = [];
+
+    const [carrinho, dispatch] = useReducer(carrinhoReducer, estadoInicial)
     const [quantidade,setQuantidade] = useState(0);
     const [valorTotal,setValorTotal] = useState(0);
 
+    const { totalTemp, quantidadeTemp } = useMemo(() => {
+        return carrinho.reduce(
+          (acumulador, produto) => ({
+            quantidadeTemp: acumulador.quantidadeTemp + produto.quantidade,
+            totalTemp: acumulador.totalTemp + produto.preco * produto.quantidade,
+          }),
+          {
+            quantidadeTemp: 0,
+            totalTemp: 0,
+          }
+        );
+      }, [carrinho]);
+    
+      useEffect(() => {
+        setQuantidade(quantidadeTemp);
+        setValorTotal(totalTemp);
+      });
+    
+
     const context = {
         carrinho,
-        setCarrinho,
+        dispatch,
         quantidade,
-        setQuantidade,
         valorTotal,
-        setValorTotal
     }
 
     return (
